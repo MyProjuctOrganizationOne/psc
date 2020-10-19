9b6ce3294a43f786caa87d1383425e8a7aefd84d
Ridhun Dev <ridhunsql@gmail.com>
2020-10-19 13:56:34 +0530

    Updates

---
+++ precache-manifest.bdee23abcf49f191a5a440369197e3f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07922265fd14f36a361dad6cd3c5deb",
+    "url": "/psc/index.html"
+  },
+  {
+    "revision": "6655d1c149400de936be",
+    "url": "/psc/static/js/2.843905dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/psc/static/js/2.843905dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b557c0544ecf39b7a0",
+    "url": "/psc/static/js/main.b89bd701.chunk.js"
+  },
+  {
+    "revision": "47494d52f37f68e0acfa",
+    "url": "/psc/static/js/runtime-main.838af4e6.js"
+  }
+]);
